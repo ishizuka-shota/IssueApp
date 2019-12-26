f2524b4549c8077b01e463ac50fb0192dab616f6
ishizuka-shota <ishizuka.shota@isid-intertech.co.jp>
2019-12-26 11:45:50 +0900

    バグ修正

--- IssueApp/Controllers/IssueController.cs
+++ IssueApp/Controllers/IssueController.cs
@@ -108,7 +108,7 @@ namespace IssueApp.Controllers
                     var issueText = string.Empty;
                     foreach (var issue in issueList)
                     {
-                        var assigneesString = issue.Assignees.Any() ? string.Join(" ", issue.Assignees) : "";
+                        var assigneesString = issue.Assignees.Any() ? string.Join(" ", issue.Assignees.Select(x => x.Name)) : "";
 
                         issueText =
                             $"```\nissueID\n{issue.Id}\nアサイン者\n{assigneesString}\nタイトル\n{issue.Title}\n本文\n{issue.Body}\n登録者?{issue.User.Name}\n状態\n{issue.State.StringValue}\nクローズ者{issue.ClosedBy.Name}\n```";
